9c8b0e06a49c682d21d1a663ea599bd815fa9511
George Witteman <georgewitteman@gmail.com>
2018-02-20 16:47:07 -0500

    Milestone 2 complete

--- code/milestone2/milestone2.ino
+++ code/milestone2/milestone2.ino
@@ -2,28 +2,74 @@
 
 int baud = 9600;
 
-int switchPin = 10;
-int servo1Pin = 22;
-int servo2Pin = 23;
+// Constants
+int switchPin = 10; // ON / OFF switch
 
-Servo servo1;
-Servo servo2;
+int leftServoPin = 22; // Continuous rotation servo for left wheel
+int rightServoPin = 23; // Continuous rotation servo for right wheel
 
+int leftSensorPin = A7; // Sensor under left wheel
+int rightSensorPin = A6; // Sensor under right wheel
+
+// The threshold for the boundary lines
+// Setting to 100 temporarily. This will also catch the in-boundary lines,
+// but we'll deal with that later
+int leftSensorBoundaryThreshold = 100;
+int rightSensorBoundaryThreshold = 500;
+
+Servo leftServo;
+Servo rightServo;
+
+// Initial setup for the Arduino
 void setup() {
   Serial.begin(baud);
   pinMode(switchPin, INPUT);
-  servo1.attach(servo1Pin);
-  servo2.attach(servo2Pin);
+  leftServo.attach(leftServoPin);
+  rightServo.attach(rightServoPin);
 }
 
+int stopped = 0;
+// Code that continuously runs on Arduino
 void loop() {
+  // If switch is in the on position, stop doing everything
   if (digitalRead(switchPin) == HIGH) {
-    Serial.print("stop\n");
-    servo1.write(90);
-    servo2.write(90);
+    stopDriving();
+    return;
+  }
+  if (stopped) {
+    return;
+  }
+
+  // Switch is in ON position. Keep doing everything...
+  if (inBounds()) {
+    drive(); // Drive if we're in bounds
   } else {
-    Serial.print("go\n");
-    servo1.write(150);
-    servo2.write(30);
+    stopped = 1;
+    stopDriving(); // Stop driving if we've hit a boundary line
   }
 }
+
+// Returns 1 if the robot is inside the boundary lines, 0 otherwise
+int inBounds() {
+  int leftSensorVal = analogRead(leftSensorPin);
+  int rightSensorVal = analogRead(rightSensorPin);
+//    Serial.println(leftSensorVal);
+//  Serial.println(analogRead(rightSensorPin));
+  return leftSensorVal > leftSensorBoundaryThreshold && rightSensorVal > rightSensorBoundaryThreshold;
+}
+
+// Drive the robot forward at a constant speed
+void drive() {
+  leftServo.write(20);
+  rightServo.write(160);
+}
+
+// Stop the robot from driving
+void stopDriving() {
+  leftServo.write(160);
+  rightServo.write(20);
+  delay(1000);
+  leftServo.write(90);
+  rightServo.write(90);
+}
+
